5ef6f6cceb5789bc7f7a0ad1bc8b63dfc4fddccf
exslym <76844562+exslym@users.noreply.github.com>
2022-01-22 10:49:50 +0300

    window resize fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,19 +22,19 @@ window.addEventListener('DOMContentLoaded', function () {
 	// });
 
 	// принудительное обновление страницы при ресайзе окна браузера (vanila JS)
-	const addEvent = function (object, type, callback) {
-		if (object == null || typeof object == 'undefined') return;
-		if (object.addEventListener) {
-			object.addEventListener(type, callback, false);
-		} else if (object.attachEvent) {
-			object.attachEvent('on' + type, callback);
-		} else {
-			object['on' + type] = callback;
-		}
-	};
-	addEvent(window, 'resize', function () {
-		window.location.reload();
-	});
+	// const addEvent = function (object, type, callback) {
+	// 	if (object == null || typeof object == 'undefined') return;
+	// 	if (object.addEventListener) {
+	// 		object.addEventListener(type, callback, false);
+	// 	} else if (object.attachEvent) {
+	// 		object.attachEvent('on' + type, callback);
+	// 	} else {
+	// 		object['on' + type] = callback;
+	// 	}
+	// };
+	// addEvent(window, 'resize', function () {
+	// 	window.location.reload();
+	// });
 
 	//плавное раскрытие списков:
 	let expandButton = document.querySelectorAll('.expand_btn'),
